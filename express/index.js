752b84eadee0531202f113860d61ff34bb844acb
leandro <leandro.lopes.dev@gmail.com>
2020-03-20 23:35:39 -0300

    trabalhando com paramentros

--- express/index.js
+++ express/index.js
@@ -9,12 +9,31 @@ app.get("/" , function(req , res){
 
 });
 
-app.get("/blog" , function(req , res){
-    res.send("Bem Vindo ao Blog")
+// paramentro não obrigatório coloca a ? na URL
+app.get("/blog/:artigo?" , function(req , res){
+
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("<h2> Artigo: " + artigo + " </h2>");
+    }else{
+        res.send("Bem Vindo ao Blog")
+    }
+    
+    
 });
 
 app.get("/canal/youtube" , function(req, res){
     res.send("Bem Vindo ao meu Canald do Youtube")
+})
+
+// rota com parametro
+app.get("/ola/:nome/:empresa" , function(req, res){
+    // req => dados enviados pelo usuário
+    // res =>  resposta que vai ser enviada para o usuario
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1> Oi " + nome + " da " + empresa + "</h1>");
 })
 
 // Servidor sempre o ultimo a criar
@@ -22,7 +41,7 @@ app.listen(4000,function(erro){
     if(erro){
         console.log("Ocorreu um Erro!")
     }else{
-        console.log("Servidor Iniciado com Sucesso!")
+        console.log("Servidor Iniciado com Sucesso e agora com Nodemon!")
     }
 })
 
