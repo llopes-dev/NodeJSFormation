5e24b2f46c85c05e3145356015fc8ff742ba6181
leandro <leandro.lopes.dev@gmail.com>
2020-03-20 23:05:04 -0300

    Criaçao de Rotas com Express

--- express/index.js
+++ express/index.js
@@ -2,7 +2,22 @@ const express = require("express"); // importando o express
 
 const app = express(); // Iniciando o express com a variavel app
 
+// Criando Rotas
+app.get("/" , function(req , res){
 
+    res.send("Bem Vindo ao Servidor")
+
+});
+
+app.get("/blog" , function(req , res){
+    res.send("Bem Vindo ao Blog")
+});
+
+app.get("/canal/youtube" , function(req, res){
+    res.send("Bem Vindo ao meu Canald do Youtube")
+})
+
+// Servidor sempre o ultimo a criar
 app.listen(4000,function(erro){
     if(erro){
         console.log("Ocorreu um Erro!")
