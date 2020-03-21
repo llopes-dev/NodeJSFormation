666f11b752fcd84600e93ae480109ec4199a1d3a
leandro <leandro.lopes.dev@gmail.com>
2020-03-20 21:07:43 -0300

    hello world

---
+++ app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "leandro.pro";
+
+
+console.log("Hello World!");
+console.log("Meu nome é Leandro e estou aprendendo NodeJs com o Guia do Programador")
+
+if(mostrarSite){
+    console.log(site);
+}
